4bbe93bdb4260d1d5e8038abb0c71a920c3367a1
Gabriel Perdue <gnperdue@gmail.com>
2019-03-09 14:50:12 -0600

    working mods while horsing around with pymc3

--- Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
+++ Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
@@ -1,36 +1,67 @@
 import sys
-
+import time
 import numpy as np
-from IPython.core.display import Image
-
 import praw
+import reddit_connect
+
+n_sub = int(sys.argv[1]) if len(sys.argv) > 1 else 3
 
 
-reddit = praw.Reddit("BayesianMethodsForHackers")
-subreddit  = reddit.get_subreddit("showerthoughts")
+def get_up_and_down_vote(sub):
+    ratio = sub.upvote_ratio
+    ups = int(round((ratio*sub.score)/(2*ratio - 1))
+              if ratio != 0.5 else round(sub.score/2))
+    downs = ups - sub.score
+    return ups, downs
 
-top_submissions = subreddit.get_top(limit=100)
 
-n_sub = int( sys.argv[1] ) if sys.argv[1] else 1
+client_id = reddit_connect.get_client_id()
+client_secret = reddit_connect.get_client_secret()
+username = reddit_connect.get_username()
+password = reddit_connect.get_passwd()
+user_agent = 'BayesHackersClient/0.1 by ' + username
 
-i = 0
-while i < n_sub:
-    top_submission = next(top_submissions)
-    i+=1
 
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    username=username,
+    password=password,
+)
+subreddit = reddit.subreddit("showerthoughts")
+
+t_stamp = time.time()
+save_npy_name = 'up_down_votes_t%d' % t_stamp
+save_contents_name = 'contents_t%d.txt' % t_stamp
+# go by timespan - 'hour', 'day', 'week', 'month', 'year', 'all'
+# might need to go longer than an hour to get entries...
+top_submissions = subreddit.top('hour', limit=5)
+top_submissions = subreddit.top('all', limit=n_sub)
+
+top_submission = next(top_submissions)
 top_post = top_submission.title
+u, d = get_up_and_down_vote(top_submission)
 
-upvotes = []
-downvotes = []
-contents = []
+upvotes = [u]
+downvotes = [d]
+contents = [top_post]
 
 for sub in top_submissions:
     try:
-        ratio = reddit.get_submission(sub.permalink).upvote_ratio
-        ups = int(round((ratio*sub.score)/(2*ratio - 1)) if ratio != 0.5 else round(sub.score/2))
-        upvotes.append(ups)
-        downvotes.append(ups - sub.score)
+        u, d = get_up_and_down_vote(sub)
+        upvotes.append(u)
+        downvotes.append(d)
         contents.append(sub.title)
     except Exception as e:
         continue
-votes = np.array( [ upvotes, downvotes] ).T
+
+votes = np.array([upvotes, downvotes]).T
+np.save(save_npy_name, votes)
+with open(save_contents_name, 'w') as f:
+    for s in contents:
+        f.write(s + '\n')
+
+print('output files = {}, {}'.format(
+    save_npy_name + '.npy', save_contents_name
+))
